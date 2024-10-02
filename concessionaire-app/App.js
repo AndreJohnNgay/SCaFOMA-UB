b12ccb77db80ffcd74c49f0aecdd694bd78a3f84
Ebby Jed Noble <20209569@s.ubaguio.edu>
2024-10-03 06:23:54 +0800

    ebby commit 10/03

--- concessionaire-app/App.js
+++ concessionaire-app/App.js
@@ -8,8 +8,8 @@ const App = () => {
 	return (
 		<GlobalProvider>
 			<StatusBar
-				barStyle="light-content" // For light text (dark-content for dark text)
-				backgroundColor="rgb(174,12,46)" // Customize the color
+				barStyle="light-content"
+				backgroundColor="rgb(174,12,46)"
 			/>
 			<NavigationContainer>
 				<AppStack />
